b0f14b488373ad6451e0feba78a5942449d2ce0c
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-04-17 05:44:19 -0500

    Running stop/cont without args finishes the exec Added 'qil' to kill a process by bundle name 	new file:   Readme.md 	new folder: qil/

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Usage
+
+To stop (pause) a process use the 'stop' binary:
+
+```
+ale@mac ~ % stop Visual Studio Code
+Stoped Electron with PID 42641
+```
+
+To resume a process use the 'cont' binary:
+
+```
+ale@mac ~ % cont Visual Studio Code
+Continued Electron with PID 42641
+```
+
+To kill a process use the 'qil' binary:
+
+```
+ale@mac ~ % qil Visual Studio Code
+Killed Electron with PID 42641
+```
+
--- qil/src/main.rs
+++ qil/src/main.rs
@@ -6,9 +6,9 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let target_name: &str = &(args[1..].join(" ").to_lowercase());
-    
-    if target_name.len() == 0 { return; }
 
+    if target_name.len() == 0 { return; }
+    
     let s = System::new_all();
     for (pid, process) in s.get_processes() {
         let process_path = process.exe().display().to_string().to_lowercase();
@@ -17,12 +17,12 @@ fn main() {
             let s = pid.to_string();
             let _s: &str = &s;
             let a = Command::new("kill")
-            .args(&["-cont", _s ])
+            .args(&["-9", _s ])
             .output()
             .expect("Fail");
             io::stdout().write_all(&a.stdout).unwrap();
             io::stderr().write_all(&a.stderr).unwrap();
-            println!("Continued {1} with PID {0}", pid, process.name());
+            println!("Killed {1} with PID {0}", pid, process.name());
             break;
         }
     }
